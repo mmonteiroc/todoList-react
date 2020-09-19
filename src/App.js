04ea75c75d4ad0549cb827e97c2342267edc8c30
MIGUEL MONTEIRO <miguelmonteiroclaveri@gmail.com>
2020-09-19 18:32:26 +0200

    Project ended with locastorage TODO app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,84 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 // Components
-import Form from './components/Form';
-import TodoList from './components/TodoList';
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  /**
+   * States
+   */
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [filter, setFilter] = useState("all");
+
+  /**
+   * Once app starts
+   */
+
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  /**
+   * Effects
+   *
+   * cada vez que TODOS(o todo lo que este dentro del array) cambia, se ejecuta este callback
+   */
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, filter]);
+
+  
+  /**
+   * Functions
+   */
+  const filterHandler = () => {
+    switch (filter) {
+      case "completed":
+        setFilteredTodos(todos.filter((indv) => indv.completed));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((indv) => !indv.completed));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos")===null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      setTodos(JSON.parse(localStorage.getItem("todos")))
+    }
+  };
+
+  const saveLocalTodos = () => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
   return (
     <div className="App">
       <header>
         <h1>Miguel's Todo List</h1>
       </header>
-      <Form/>
-      <TodoList/>
+      <Form
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setFilter={setFilter}
+      />
+
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React from "react";
 
-const Form = () =>{
+const Form = ({setInputText, setTodos, todos, inputText, setFilter}) => {
+  /**
+   * JS Functions and code
+   */
+  const inputHandler = (e) => {
+    setInputText(e.target.value)
+  };
 
-    return (
-        <form>
-            <input type="text" className="todo-input" />
-            <button className="todo-button" type="submit">
-            <i className="fas fa-plus-square"></i>
-            </button>
-            <div className="select">
-            <select name="todos" className="filter-todo">
-                <option value="all">All</option>
-                <option value="completed">Completed</option>
-                <option value="uncompleted">Uncompleted</option>
-            </select>
-            </div>
-        </form>
-    );
-}
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+        ...todos,
+        {
+            text: inputText,
+            completed: false,
+            id: Math.random()*1000
+        }
+    ]);
+    /**
+     * Set the input clear
+     */
+    setInputText("")
+  }
+
+  const filterHandler = (e) => {
+    setFilter(e.target.value)
+  }
+  
+  return (
+    <form>
+      <input type="text" className="todo-input" onChange={inputHandler} value={inputText}/>
+      <button className="todo-button" type="submit" onClick={submitHandler}>
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select name="todos" className="filter-todo" onChange={filterHandler}>
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
+  );
+};
 
 export default Form;
